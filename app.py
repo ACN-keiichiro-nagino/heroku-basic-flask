81f8c535d4442453ec4ef1c29d6152c7921c1531
ACN-keiichiro-nagino <keiichiro.nagino@accenture.com>
2020-07-08 11:00:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 from flask import Flask
-from datetime import datetime
 import http.client
+import mimetypes
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-
-    headers = {'client_id': 'test','client_secret': 'test'}
-
-    conn = http.client.HTTPConnection('test-exp-heroku.us-e2.cloudhub.io')
-    conn.request('GET','/api/test/A00000000000000001', headers)
+    
+    conn = http.client.HTTPConnection("mule-cicd-demo-dev1.us-e2.cloudhub.io")
+    payload = ''
+    headers = {}
+    conn.request("GET", "/api/student", payload, headers)
     res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
 
-    data = res.read().decode('utf-8')
-    print(res.status, res.reason)
-    print(res.getheaders())
-
-    return data
+    return data.decode("utf-8")
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
-
-
-
 
