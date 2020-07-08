3d509b8d17cd6741c4a3569cf41d132390ea98ef
Keiichiro Nagino <keiichiro.nagino@imjp.co.jp>
2020-07-08 10:43:35 +0900

    fix

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask
 from datetime import datetime
+import http.client
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    headers = {'client_id': 'test','client_secret': 'test'}
 
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    conn = http.client.HTTPConnection('test-exp-heroku.us-e2.cloudhub.io')
+    conn.request('GET','/api/test/A00000000000000001', headers)
+    res = conn.getresponse()
+
+    data = res.read().decode('utf-8')
+    print(res.status, res.reason)
+    print(res.getheaders())
+
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
+
+
+
+
 
